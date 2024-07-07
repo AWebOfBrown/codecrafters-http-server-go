2dae9c0bb32da281810fafbc661eae4dd2bd66a7
awebofbrown <ajcbrown820@gmail.com>
2024-07-07 16:26:19 +1000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,13 +15,45 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Connected \n")
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	buff := bufio.NewReader(conn)
+	reqLine, err := buff.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error reading connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	responseContent := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	fmt.Printf("req string: %q \n", reqLine)
 
-	conn.Write(responseContent)
+	split := strings.Split(reqLine, " ")
+
+	addr := split[1]
+	fmt.Printf("addr: %q \n", addr)
+
+	var responseContent []byte
+	if addr == "/" {
+		responseContent = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		responseContent = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
+	fmt.Printf("responseContent: %q", responseContent)
+
+	n, err := conn.Write(responseContent)
+	if err != nil {
+		fmt.Printf("err: ", err)
+	}
+	fmt.Printf("bytes: %d \n", n)
+	conn.Close()
 }
