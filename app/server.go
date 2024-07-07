844a937e89fd65bc2e7f82a2299af716977a6f35
awebofbrown <ajcbrown820@gmail.com>
2024-07-07 15:38:03 +1000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -13,9 +13,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	responseContent := []byte("HTTP/1.1 200 OK\r\n\r\n")
+
+	conn.Write(responseContent)
 }
