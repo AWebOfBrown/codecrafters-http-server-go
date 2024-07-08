aa1ec2d0b9a4aaf3309b1d3f7e120bf257fc900b
awebofbrown <ajcbrown820@gmail.com>
2024-07-08 13:31:25 +1000

    codecrafters submit [skip ci]

--- app/echo.go
+++ app/echo.go
@@ -9,11 +9,19 @@ func handle_echo(req *Request, res *Response) {
 	splitAddr := strings.Split(req.Path, "/")
 	responseString := splitAddr[2]
 
+	var contentEncoding string = ""
+	if req.Headers["Accept-Encoding"] != "" {
+		if req.Headers["Accept-Encoding"] == "gzip" {
+			contentEncoding = "gzip"
+		}
+	}
+
 	res.Status = 200
 	res.Message = "OK"
 	res.Headers = map[string]string{
-		"Content-Type":   "text/plain",
-		"Content-Length": strconv.Itoa(len(responseString)),
+		"Content-Type":     "text/plain",
+		"Content-Length":   strconv.Itoa(len(responseString)),
+		"Content-Encoding": contentEncoding,
 	}
 	res.Body = []byte(responseString)
 }
